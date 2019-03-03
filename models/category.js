8661fb07011f5692867a976a80fc3207ac073342
MichaelMulat <33102250+MichaelMulat@users.noreply.github.com>
2019-03-03 00:32:03 -0500

    Add files via upload

---
+++ models/category.js
@@ -0,0 +1,9 @@
+module.exports = function (sequelize, DataTypes) {
+  var Categories = sequelize.define("Category", {
+    category: DataTypes.STRING
+  });
+  Categories.associate = function (models) {
+    Categories.hasMany(models.Timeline);
+  };
+  return Categories;
+};
